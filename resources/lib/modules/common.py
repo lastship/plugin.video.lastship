b7b09e5931be922cf228ffc3fc034957327dd4bd
anis3 <anis3.n@web.de>
2020-10-02 05:05:32 +0000

    Update common.py

--- resources/lib/modules/common.py
+++ resources/lib/modules/common.py
@@ -1,7 +1,6 @@
-# -*- coding: UTF-8 -*-
-
+# -*- coding: utf-8 -*-
 """
-    Lastship Add-on (C) 2019
+    Lastship Add-on (C) 2020
     Credits to Placenta and Covenant; our thanks go to their creators
 
     This program is free software: you can redistribute it and/or modify
@@ -22,19 +21,15 @@
 # Addon id: plugin.video.lastship
 # Addon Provider: LastShip
 
-import xbmcaddon
-from xbmc import translatePath
-
+import xbmcaddon, sys
 addonID = 'plugin.video.lastship'
-addon = xbmcaddon.Addon(id = addonID)
+addon = xbmcaddon.Addon(addonID)
 addonName = addon.getAddonInfo('name')
-addonPath = translatePath(addon.getAddonInfo('path')).decode('utf-8')
-profilePath = translatePath(addon.getAddonInfo('profile')).decode('utf-8')
-
-IE_USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko'
-FF_USER_AGENT = 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0'
-OPERA_USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36 OPR/34.0.2036.50'
-IOS_USER_AGENT = 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25'
-ANDROID_USER_AGENT = 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36'
-
-UA_LIST = [IE_USER_AGENT, FF_USER_AGENT, OPERA_USER_AGENT, IOS_USER_AGENT, ANDROID_USER_AGENT]
+if sys.version_info[0] == 2:
+    from xbmc import translatePath
+    addonPath = translatePath(addon.getAddonInfo('path')).decode('utf-8')
+    profilePath = translatePath(addon.getAddonInfo('profile')).decode('utf-8')
+else:
+    from xbmcvfs import translatePath
+    addonPath = translatePath(addon.getAddonInfo('path'))
+    profilePath = translatePath(addon.getAddonInfo('profile'))
